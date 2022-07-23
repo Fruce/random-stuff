49f77ac6f7247eaa5f20e3251cb9b24424fa9fa7
Fruce <89212231+Fruce@users.noreply.github.com>
2022-07-23 14:14:52 +0530

    Update digit_increment.py

--- digit_increment.py
+++ digit_increment.py
@@ -1,5 +1,6 @@
 x = input("enter a word: ")
 num =[]
+zeros = []
 for i in range(1,len(x)+1):
 	try:
 		if type(int(x[-i])) == type(1):
@@ -9,4 +10,14 @@ for i in range(1,len(x)+1):
 if len(num)==0:
 	print(x+'1')
 else:
-    print(x[:len(x)-len(num)]+str(int(''.join(num[::-1]))+1))
+    for i in num[::-1]:
+        try:
+            if i == '0':
+                zeros.append(i)
+        except:
+            break
+    sum = int(''.join(num[::-1]))+1
+    if len(num) != len(''.join(zeros)+str(sum)):
+        print(x[:len(x)-len(num)]+''.join(zeros[:len(zeros)-1])+str(sum))
+    else:
+        print(x[:len(x)-len(num)]+''.join(zeros)+str(sum))
