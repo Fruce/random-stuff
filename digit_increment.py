792d20acf6467945736f4f221cb12f2cbf3fe62f
Fruce <89212231+Fruce@users.noreply.github.com>
2022-07-23 14:32:54 +0530

    Update digit_increment.py

--- digit_increment.py
+++ digit_increment.py
@@ -1,23 +1,32 @@
-x = input("enter a word: ")
 num =[]
 zeros = []
-for i in range(1,len(x)+1):
-	try:
-		if type(int(x[-i])) == type(1):
-			num.append(x[-i])		
-	except:		
-		break
+
+def num_extract(x):
+    for i in range(1,len(x)+1):
+        try:
+            if type(int(x[-i])) == type(1):
+                num.append(x[-i])		
+        except:		
+            break
+
+def zero_extract(num):
+    for i in num[::-1]:
+        if i == '0':
+            zeros.append(i)
+        else:
+            break
+    print(zeros)
+    return str(int(''.join(num[::-1]))+1)
+
+#driver code
+x = input("enter a word: ")
+num_extract(x)
+sum = zero_extract(num)
+
 if len(num)==0:
 	print(x+'1')
 else:
-    for i in num[::-1]:
-        try:
-            if i == '0':
-                zeros.append(i)
-        except:
-            break
-    sum = int(''.join(num[::-1]))+1
-    if len(num) != len(''.join(zeros)+str(sum)):
-        print(x[:len(x)-len(num)]+''.join(zeros[:len(zeros)-1])+str(sum))
+    if len(num) != len(''.join(zeros)+sum):
+        print(x[:len(x)-len(num)]+''.join(zeros[:len(zeros)-1])+sum)
     else:
-        print(x[:len(x)-len(num)]+''.join(zeros)+str(sum))
+        print(x[:len(x)-len(num)]+''.join(zeros)+sum)
