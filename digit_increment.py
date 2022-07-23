f9f72aa721387a4907cf10c7e138e0590682820f
Fruce <89212231+Fruce@users.noreply.github.com>
2022-07-23 14:50:40 +0530

    Update digit_increment.py

--- digit_increment.py
+++ digit_increment.py
@@ -1,13 +1,13 @@
 num =[]
 zeros = []
-def num_extract(x):
+def num_extract():
     for i in range(1,len(x)+1):
         try:
-            if type(int(x[-i])) == type(1):
+            if isinstance(int(x[-i]), int) is True:
                 num.append(x[-i])		
         except:
             break
-def zero_extract(num):
+def zero_extract():
     for i in num[::-1]:
         if i == '0':
             zeros.append(i)
@@ -16,14 +16,13 @@ def zero_extract(num):
     return str(int(''.join(num[::-1]))+1)
 
 x = input("enter a word: ")
-num_extract(x)
+num_extract()
 
 if len(num)==0:
 	print(x+'1')
 else:
-    sum = zero_extract(num)
+    sum = zero_extract()
     if len(num) != len(''.join(zeros)+sum):
         print(x[:len(x)-len(num)]+''.join(zeros[:len(zeros)-1])+sum)
     else:
         print(x[:len(x)-len(num)]+''.join(zeros)+sum)
-
